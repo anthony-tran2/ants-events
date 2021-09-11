156ac2bcd901fcf395f7fa044b8b027a2510073a
Anthony Tran <anthony.tran6264@yahoo.com>
2021-09-11 01:01:29 +0000

    Completed finishing touches on Feature 2

--- client/pages/create-event-page.jsx
+++ client/pages/create-event-page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import EventForm from '../components/event-form';
-import { Container, Typography, makeStyles, Grid } from '@material-ui/core';
+import { Container, Typography, makeStyles, Grid, useMediaQuery } from '@material-ui/core';
 import BackButton from '../components/back-button';
 
 const useStyles = makeStyles(theme => (
@@ -12,7 +12,10 @@ const useStyles = makeStyles(theme => (
     },
 
     absolute: {
-      position: 'absolute',
+      position: 'absolute'
+    },
+
+    height: {
       height: '2.5rem'
     }
   }
@@ -20,11 +23,12 @@ const useStyles = makeStyles(theme => (
 
 export default function CreateEvent(props) {
   const classes = useStyles();
+  const matches = useMediaQuery('(min-width:500px)');
   return (
     <>
     <main>
       <Container maxWidth="lg" >
-          <Grid container className={classes.absolute} alignItems='center'>
+          <Grid container className={`${matches ? classes.absolute : ''} ${classes.height}`} alignItems='center'>
           <BackButton />
         </Grid>
         <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom className={classes.heading}>
